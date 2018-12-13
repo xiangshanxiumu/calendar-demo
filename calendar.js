07b58e7c690a0733aa1e03d3d0242ad5c02d95ca
xiangshanxiumu <17317852392@163.com>
2018-12-13 21:08:07 +0800

    完善实时时间渲染

--- calendar.js
+++ calendar.js
@@ -1,14 +1,11 @@
-(function(){//IIFE
-    window.Calendar = Calendar;//挂载到window下面
- 
-	//申明Calendar的构造函数
+(function(){
+    window.Calendar = Calendar;
+
     function Calendar(JSON){
-    	//this.inputBox = null;//存放当前日期
-		this.calendarDiv = null;//存放顶部 年 月
-		this.calendarSelectBox = null;
+    	this.inputBox = null;//存放当前日期
+    	this.calendarDiv = null;//存放顶部 年 月
     	this.yearSelect = null;
-		this.monthSelect = null;
-		this.holidaySelect = null;
+    	this.monthSelect = null;
     	this.tds = null;
     	this.year = (new Date()).getFullYear();
     	this.month = (new Date()).getMonth()+1;
@@ -16,16 +13,17 @@
 
     	this.init(JSON);
 
-		this.getDay(this.year,this.month,this.date);
-		
-		this.getTime();
+    	this.getDay(this.year,this.month,this.date);
         //绑定事件
     	this.bindEvent();
     }
 
     Calendar.prototype.init = function(JSON){
-    	this.dom = document.getElementById(JSON["id"]);//获取dom入口节点
-
+    	this.dom = document.getElementById(JSON["id"]);
+        // console.log(this.dom);
+    	this.inputBox = document.createElement("input");
+    	this.inputBox.type = "text";
+    	this.dom.appendChild(this.inputBox);
         this.calendarDiv = document.createElement("div");
         this.calendarDiv.className = "calendarDiv";
         this.dom.appendChild(this.calendarDiv);
@@ -33,42 +31,50 @@
         //日历选择栏 年份、月份等选择
         this.calendarSelectBox =document.createElement("div");
         this.calendarSelectBox.className = "calendarSelectBox";
-        this.calendarDiv.appendChild(this.calendarSelectBox);
-
+		this.calendarDiv.appendChild(this.calendarSelectBox);
+		
+		// 下拉年份外包裹盒子
+		this.warpBox1 = document.createElement("div")
+		this.calendarSelectBox.appendChild(this.warpBox1)
         //下拉年份
         this.yearSelect = document.createElement("select");
-        this.calendarSelectBox.appendChild(this.yearSelect);
+        this.warpBox1.appendChild(this.yearSelect);
         for(var i=1990;i<=2050;i++){
         	var option = document.createElement("option");
         	option.value = i;
         	option.innerHTML = i+"年";
         	this.yearSelect.appendChild(option);
         }
-
+		//下拉月份和左右箭头外盒子
+		this.wrapBox2 = document.createElement("div")
+		this.calendarSelectBox.appendChild(this.wrapBox2);
+		
         //左箭头
-        this.leftArrow = document.createElement("p");
+        this.leftArrow = document.createElement("button");
         this.leftArrow.innerHTML = "&lt;";
-        this.calendarSelectBox.appendChild(this.leftArrow);
+        this.wrapBox2.appendChild(this.leftArrow);
 
         //下拉月份
         this.monthSelect = document.createElement("select");
-        this.calendarSelectBox.appendChild(this.monthSelect);
+        this.wrapBox2.appendChild(this.monthSelect);
         var monthArr = ["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
-        for (var i =0; i <monthArr.length; i++) {
+        for (var i = 1; i <=monthArr.length; i++) {
         	var option = document.createElement("option");
-        	option.value = i+1;
-        	option.innerHTML = monthArr[i];
+        	option.value = i;
+        	option.innerHTML =monthArr[i-1];
         	this.monthSelect.appendChild(option);
         }
         
          //右箭头
-        this.rightArrow = document.createElement("p");
+        this.rightArrow = document.createElement("button");
         this.rightArrow.innerHTML = "&gt;";
-        this.calendarSelectBox.appendChild(this.rightArrow);
-
+        this.wrapBox2.appendChild(this.rightArrow);
+		// 下拉假期外包裹盒子
+		this.wrapBox3 = document.createElement("div")
+		this.calendarSelectBox.appendChild(this.wrapBox3);
         //假期选择
         this.holidaySelect = document.createElement("select");
-        this.calendarSelectBox.appendChild(this.holidaySelect);
+        this.wrapBox3.appendChild(this.holidaySelect);
         var holidayArr = ["假期安排","元旦","除夕","春节","清明节","劳动节","端午节","中秋节","国庆节"];
         for(var i=0;i<holidayArr.length;i++){
         	var option = document.createElement("option");
@@ -76,17 +82,19 @@
         	option.innerHTML = holidayArr[i];
         	this.holidaySelect.appendChild(option);
         }
-
+		// 返回今天-外包裹盒子
+		this.wrapBox4 = document.createElement("div")
+		this.calendarSelectBox.appendChild(this.wrapBox4);
         //返回今天
-        this.backToday = document.createElement("p");
-        this.calendarSelectBox.appendChild(this.backToday);
+        this.backToday = document.createElement("button");
+        this.wrapBox4.appendChild(this.backToday);
         this.backToday.innerHTML = "返回今天";
         
         //日历显示表格
         this.tableDom = document.createElement("table")
         this.calendarDiv.appendChild(this.tableDom);
 
-        var weekArr = ["周日","周一","周二","周三","周四","周五","周六"];
+        var weekArr = ["日","一","二","三","四","五","六"];
         this.tr = document.createElement("tr");
 
         for(var i=0;i<weekArr.length;i++){
@@ -111,74 +119,51 @@
         this.rightBox = document.createElement("div");
         this.rightBox.className = "rightBox";
         this.dom.appendChild(this.rightBox);
+		// 年月日-星期外包裹盒子
+		this.wrapBox5 = document.createElement("div")
+		this.wrapBox5.className = "wrapBox5";
+		this.rightBox.appendChild(this.wrapBox5);
 
-        this.dateTxt = document.createElement("p");//日期 x年x月x日
+        this.dateTxt = document.createElement("div");//日期 x年x月x日
         this.dateTxt.className = "dateTxt";
-        this.rightBox.appendChild(this.dateTxt);
+        this.wrapBox5.appendChild(this.dateTxt);
 
-        this.dayTxt = document.createElement("p");//星期几
+        this.dayTxt = document.createElement("div");//星期几
         this.dayTxt.className = "dayTxt";
-        this.rightBox.appendChild(this.dayTxt);
+        this.wrapBox5.appendChild(this.dayTxt);
 
         this.currentDay = document.createElement("div"); //当前日期 几号？
         this.currentDay.className = "currentDay";
-        this.rightBox.appendChild(this.currentDay);
+		this.rightBox.appendChild(this.currentDay);
 		
-		this.currentTimeBox = document.createElement("div")//当前时间 时-分-秒
-		this.currentTimeBox.className = "currentTimeBox";
-		this.rightBox.appendChild(this.currentTimeBox);
-		
-		this.currentTime = document.createElement("div");
-		this.currentTime.className = "currentTime";
-		this.currentTimeBox.appendChild(this.currentTime);
-        //小时
-		hour = document.createElement("span");
-		hour.className = "hour";
-		this.currentTime.appendChild(hour);
-		//时
-		HOUR = document.createElement("span");
-		HOUR.innerHTML = "时";
-		this.currentTime.appendChild(HOUR);
-        //分钟
-		minute = document.createElement("span");
-		minute.className = "minute";
-		this.currentTime.appendChild(minute);
-		//分
-		MINUTE = document.createElement("span");
-		MINUTE.innerHTML = "分";
-		this.currentTime.appendChild(MINUTE);		
-        //秒
-		second = document.createElement("span");
-		second.className = "second";
-		this.currentTime.appendChild(second);
-		//秒
-		SECOND = document.createElement("span");
-		SECOND.innerHTML = "秒";
-		this.currentTime.appendChild(SECOND);		
+		// 右侧盒子实时-时-分-秒
+		this.ulTime = document.createElement("ul")
+		this.ulTime.className = "ulTime"
+		this.rightBox.appendChild(this.ulTime)
+        var k = 0
+		while(k<9){
+		li = document.createElement("li")
+		li.innerHTML = k
+		this.ulTime.appendChild(li)
+		k++
+		}
     }
-	//获取当前小时-分钟-秒
-	Calendar.prototype.getTime = function(){
-		setInterval(function(){
-			const nowTime = new Date();
-			let nowHour = nowTime.getHours();
-			let nowMinute = nowTime.getMinutes();
-			let nowSecond = nowTime.getSeconds();
-			hour.innerHTML = nowHour;
-			minute.innerHTML = nowMinute;
-			second.innerHTML = nowSecond;
-		},1000);
-	}
+
     //获取日期
     Calendar.prototype.getDay = function(year,month,date){
          this.year = year;
          this.month = month;
-		 //date&&(this.date = date);
-		 this.date = date;
+         date&&(this.date = date);
+
          this.yearSelect.value = this.year;
          this.monthSelect.value = this.month;
 
          //获取当前月第一天 是周几？利用Date()对象属性
          var thisMonthFirstDate = this.thisMonthFirstDate = new Date(year,month-1,1).getDay();
+         //console.log(month);
+         //console.log(new Date(year,month-1,1).getDay());
+         //console.log(new Date(year,month-1,1));
+         
          //上一个末尾时间戳  获取上月多少天？
          var prevMonthSumDate = this.prevMonthSumDate = new Date(new Date(year,month-1,1)-1).getDate();
          //获取本月多少天？
@@ -213,19 +198,27 @@
            this.tds[i].innerHTML = prevMonthSumDate-thisMonthFirstDate+1+i;
            this.tds[i].className = "col";
          }
+
          //渲染本月
          for(var i=thisMonthFirstDate;i<thisMonthSumDate+thisMonthFirstDate;i++){
-			this.tds[i].className = "";
-			if(this.tds[i].innerHTML==date){
-				this.tds[i].className = "cur";
-			 }
+         	this.tds[i].className = "";
          	this.tds[i].innerHTML = i-thisMonthFirstDate+1;
          }
+
          //渲染下月
          for(var i=thisMonthFirstDate+thisMonthSumDate;i<42;i++){
          	this.tds[i].innerHTML = i-(thisMonthFirstDate+thisMonthSumDate)+1;
          	this.tds[i].className = "col";
          }
+
+         //渲染input文本里面 年-月-日
+         if(date!=undefined){
+         	// console.log(this.thisMonthFirstDate+date-1);
+         	// console.log(this.thisMonthFirstDate);
+         	// console.log(date);
+            this.tds[this.thisMonthFirstDate+date-1].className = "cur";
+            this.inputBox.value = year+"-"+month+"-"+date;
+         	}
             
          //渲染右侧盒子里面 年-月-日 星期几？
          this.dateTxt.innerHTML = year+"-"+month+"-"+date;//年-月-日？
@@ -235,14 +228,32 @@
          this.dayTxt.innerHTML = weekArr[n];//星期几？
 
          this.currentDay.innerHTML = date;//几号？
-
+		 //渲染实时 时-分-秒
+		    var lis = this.ulTime.children
+			// console.log(lis)
+			lis[2].innerText = "时";
+			lis[5].innerText = "分";
+			lis[8].innerText = "秒";
+		 timer = setInterval(function(){
+			var DATE = new Date();
+			var hour = DATE.getHours();
+			var minute = DATE.getMinutes();
+			var second = DATE.getSeconds();
+			lis[0].innerText = parseInt(hour/10);
+			lis[1].innerText = hour%10;
+			lis[3].innerText = parseInt(minute/10);
+			lis[4].innerText = minute%10;
+			lis[6].innerText = parseInt(second/10);
+			lis[7].innerText = second%10;
+		 },1000);
          //渲染假期和非假期显示效果
          if((this.month==1&&this.date==1)||(this.month==2&&this.date==15)||(this.month==2&&this.date==16)||(this.month==4&&this.date==5)
          	 ||(this.month==5&&this.date==1)||(this.month==6&&this.date==18)||(this.month==9&&this.date==24)||(this.month==10&&this.date==1)){
+
 			this.calendarDiv.className = "calendarDivHoliday";  
 			this.rightBox.className = "rightBoxHoliday";
             } else{
-			this.holidaySelect.value = 0;
+            this.holidaySelect.value = 0;
 			this.calendarDiv.className = "calendarDiv";
 			this.rightBox.className = "rightBox";
             }   	
@@ -250,18 +261,23 @@
     
     // 绑定事件
     Calendar.prototype.bindEvent =function(){
+
     	var self = this;
     	//下拉年份事件
     	this.yearSelect.onchange = function () {
+    		self.inputBox.value = ""
     		self.getDay(parseInt(self.yearSelect.value),parseInt(self.monthSelect.value),self.date);
     	}
         //下拉月份事件
     	this.monthSelect.onchange = function(){
+    		self.inputBox.value = ""
     		self.getDay(parseInt(self.yearSelect.value),parseInt(self.monthSelect.value),self.date);
     	}
+
     	//左箭头事件
     	//var n =self.monthSelect.value;
     	this.leftArrow.onclick = function(){
+    		self.inputBox.value = "";
     	    self.monthSelect.value--;
     	    if(self.monthSelect.value<1){
     	    	self.monthSelect.value = 12
@@ -269,23 +285,26 @@
     	    }
         	self.getDay(parseInt(self.yearSelect.value),parseInt(self.monthSelect.value),self.date);
         }
+
     	//右箭头事件 
     	this.rightArrow.onclick = function(){
+    		self.inputBox.value = ""
     		self.monthSelect.value++;
-    	    if(self.monthSelect.value>12||self.monthSelect.value==""){//>12时 自加出现空值 那就加上空值判断解决
-				self.yearSelect.value++;
-				self.monthSelect.value = 1; 	
-			}
-			//console.log(self.monthSelect)
-    	    //console.log(self.monthSelect.value);//
+    	    if(self.monthSelect.value>12||self.monthSelect.value==""){//>12时 自加没有执行 if语句也就没有执行？ 后面代码报错
+    	    	self.monthSelect.value = 1;
+    	    	self.yearSelect.value++;
+    	    }
+    	    // console.log(self.monthSelect.value);//>12时 空输出？
         	self.getDay(parseInt(self.yearSelect.value),parseInt(self.monthSelect.value),self.date);
+
     	}
+
     	//返回今天事件
     	this.backToday.onclick = function(){
     		var y = new Date().getFullYear();
     		var m =new Date().getMonth()+1;
     		var d =new Date().getDate();
-			self.getDay(y,m,d);
+    		self.getDay(y,m,d);
     	}
 
     	//假期选择事件
@@ -293,60 +312,97 @@
             //console.log(self.holidaySelect.value);
             var y = parseInt(self.yearSelect.value);
             var m = parseInt(self.monthSelect.value);
-            var d = self.date;
-			var h = self.holidaySelect.value;
-            switch(h){
-				case "0":
-				self.backToday.onclick();
+            var d = parseInt(self.date);
+			var h = parseInt(self.holidaySelect.value);
+			console.log(typeof(h))
+            switch (h) {
+				case 0:
+				  self.backToday.onclick();
 				break;
-				case "1":
-                m =1;
-            	d= 1;
-				return self.getDay(y,m,d);
+				case 1:
+				  m =1;
+            	  d= 1;
+				  self.getDay(y,m,d);
 				break;
-				case "2":
-                m =2;
-            	d= 15;
-				self.getDay(y,m,d);
+				case 2:
+				  m =2;
+				  d= 15;
+				  self.getDay(y,m,d);
 				break;
-				case "3":
-				m =2;
-				d= 16;
-				self.getDay(y,m,d);
+				case 3:
+				  m =2;
+				  d= 16;
+				  self.getDay(y,m,d);
 				break;
-				case "4":
-				m =4;
-				d= 5;
-				self.getDay(y,m,d);
+				case 4:
+				  m =4;
+				  d= 5;
+				  self.getDay(y,m,d);
 				break;
-				case "5":
-				m =5;
-				d= 1;
-				self.getDay(y,m,d);
+				case 5:
+				  m =5;
+				  d= 1;
+				  self.getDay(y,m,d);
 				break;
-				case "6":
-				m =6;
-				d= 18;
-				self.getDay(y,m,d);
+				case 6:
+				  m =6;
+				  d= 18;
+				  self.getDay(y,m,d);
 				break;
-				case "7":
-				m =9;
-				d= 24;
-				self.getDay(y,m,d);
+				case 7:
+				  m =9;
+				  d= 24;
+				  self.getDay(y,m,d);
 				break;
-				case "8":
-            	 m =10;
-            	 d= 1;
-            	 self.getDay(y,m,d);
-				break;      
+				case 8:
+				  m =10;
+				  d= 1;
+				  self.getDay(y,m,d);
+				break;
+				default:
+				  self.backToday.onclick();
+			}              
+    	}
+
+    	//输入框获取焦点 显示日历
+       this.inputBox.onfocus = function() {
+           self.calendarDiv.style.display = "block";//左侧日历框
+           self.rightBox.style.display = "block";//右侧盒子
+       }
+       // 点击除日历之外的地方隐藏日历
+       document.addEventListener("click",function(event) {
+             var event = window.event || event;
+             //console.log(event.target);
+             if(event.target!=self.calendarDiv&&event.target != self.inputBox && event.target!=self.yearSelect && event.target!=
+                 self.monthSelect && event.target.nodeName != "TD" && event.target.nodeName !="TH"&&event.target!=
+                 self.holidaySelect&&event.target!=self.backToday&&event.target!=self.leftArrow&&event.target!=self.rightArrow&&event.target!=
+                 self.rightBox&&event.target!=self.dateTxt&&event.target!=self.dayTxt&&event.target!=self.currentDay&event.target!=self.calendarSelectBox) {
+                      self.calendarDiv.style.display = "none";//隐藏左侧日历框
+                      self.rightBox.style.display = "none";//隐藏右侧盒子
+                 }
+                 return false;
+       },false);
+
+        //从输入框输入数字 匹配日历对应的日期
+    	self.inputBox.onkeyup = function(event){
+    		var event = window.event || event;
+    		if(event.keyCode==13){
+    		var val = this.value;
+    		var valArr = val.match(/(\d+)-(\d+)-(\d+)/);
+    		if(valArr){
+    			var year = parseInt(valArr[1]);
+    			var month = parseInt(valArr[2]);
+    			var date = parseInt(valArr[3]);
+    			self.getDay(year,month,date);
+    		}
+    	}
     	}
 
     	//给所有的tds绑定事件监听
     	for(var i=0;i<self.tds.length;i++){
     		(function(i){
     			self.tds[i].onclick = function(){
-					var temp = i;//记住i的值
-					//self.tds[tem].className = "cur";
+    				var temp = i;//记住i的值
     				
     				if(temp<self.thisMonthFirstDate){
     					if(self.month<=1){
@@ -381,9 +437,7 @@
     					}
     				}
     			}
-
     		})(i);
     	}
-	}
     }
 })();
